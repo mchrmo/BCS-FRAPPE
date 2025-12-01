c38dbdf44c50fd47892bccaa03d5ca5bc051b3ce
AndrejCernak <andrej.cernak2007@gmail.com>
2025-12-01 11:14:15 +0000

    mail

--- bcservices/bcservices/doctype/klient/klient.py
+++ bcservices/bcservices/doctype/klient/klient.py
@@ -1,9 +1,36 @@
-# Copyright (c) 2025, Focus Hub s.r.o and contributors
-# For license information, please see license.txt
-
-# import frappe
+# klient.py
+import frappe
 from frappe.model.document import Document
 
-
 class Klient(Document):
-	pass
+    def after_insert(self):
+        # email klienta
+        email = self.email
+        
+        # heslo vygenerované klient scriptom (musí byť uložené v doctype)
+        password = self.heslo
+
+        # predmet
+        subject = "Vaše prihlasovacie údaje"
+
+        # telo emailu
+        message = f"""
+Dobrý deň {self.username},
+
+boli vám vytvorené prihlasovacie údaje.
+
+📧 Email: {email}
+🔐 Heslo: {password}
+
+Prosím, prihláste sa do systému a heslo si zmeňte.
+
+S pozdravom,
+Váš tím
+"""
+
+        # odoslanie emailu
+        frappe.sendmail(
+            recipients=email,
+            subject=subject,
+            message=message
+        )
