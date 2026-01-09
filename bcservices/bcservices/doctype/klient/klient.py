30012c2e69d7f981eddaf59362329940f602acc0
AndrejCernak <165067032+AndrejCernak@users.noreply.github.com>
2026-01-09 20:31:53 +0100

    Refactor welcome email formatting in klient.py

--- bcservices/bcservices/doctype/klient/klient.py
+++ bcservices/bcservices/doctype/klient/klient.py
@@ -1,36 +1,36 @@
-# klient.py
 import frappe
 import html
 from frappe.model.document import Document
 
 class Klient(Document):
     def after_insert(self):
+        # Pri vytvorení automaticky odošleme mail
+        self.send_welcome_email()
+
+    @frappe.whitelist() # Whitelist umožní volanie metódy z frontendu (tlačidlom)
+    def send_welcome_email(self):
         email = self.email
+        if not email or not self.heslo:
+            frappe.msgprint("Klient nemá vyplnený email alebo heslo.")
+            return
 
-        # pôvodné heslo
-        password_raw = self.heslo
-
-        # HTML-safe verzia hesla (nutné pre email)
-        password_safe = html.escape(password_raw or "")
+        # HTML-safe verzia hesla
+        password_safe = html.escape(self.heslo or "")
 
         subject = "Vaše prihlasovacie údaje"
-
         message = f"""
-Dobrý deň {self.username},
-
-boli vám vytvorené prihlasovacie údaje.
-
-📧 Email: {email}
-🔐 Heslo: {password_safe}
-
-Prosím, prihláste sa do systému a heslo si zmeňte.
-
-S pozdravom,
-Váš tím
-"""
+        Dobrý deň {self.username or 'používateľ'},<br><br>
+        boli vám vytvorené prihlasovacie údaje do systému.<br><br>
+        📧 <b>Email:</b> {email}<br>
+        🔐 <b>Heslo:</b> {password_safe}<br><br>
+        Prosím, prihláste sa do systému a heslo si v prípade potreby zmeňte.<br><br>
+        S pozdravom,<br>
+        Váš tím
+        """
 
         frappe.sendmail(
             recipients=email,
             subject=subject,
-            message=message
+            message=message,
+            now=True # Odošle mail hneď, nečaká na scheduler
         )
