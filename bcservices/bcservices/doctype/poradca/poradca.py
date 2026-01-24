d70d372103a6453ea9aaebcbb4ec2ae277ac4f52
AndrejCernak <andrej.cernak2007@gmail.com>
2026-01-24 15:49:54 +0000

    poradca email

--- bcservices/bcservices/doctype/poradca/poradca.py
+++ bcservices/bcservices/doctype/poradca/poradca.py
@@ -1,9 +1,35 @@
-# Copyright (c) 2026, Focus Hub s.r.o and contributors
-# For license information, please see license.txt
-
-# import frappe
+import frappe
+import html
 from frappe.model.document import Document
 
-
 class Poradca(Document):
-	pass
+    def after_insert(self):
+        self.send_welcome_email()
+
+    @frappe.whitelist()
+    def send_welcome_email(self):
+        if not self.email or not self.heslo:
+            frappe.msgprint("Poradca nemá vyplnený email alebo heslo.")
+            return
+
+        password_safe = html.escape(self.heslo)
+
+        subject = "Vaše prihlasovacie údaje – Poradca"
+        message = f"""
+        Dobrý deň,<br><br>
+        boli vám vytvorené prihlasovacie údaje do systému.<br><br>
+
+        📧 <b>Email:</b> {self.email}<br>
+        🔐 <b>Heslo:</b> {password_safe}<br><br>
+
+        Prosím, po prihlásení si heslo zmeňte.<br><br>
+        S pozdravom,<br>
+        Váš tím
+        """
+
+        frappe.sendmail(
+            recipients=self.email,
+            subject=subject,
+            message=message,
+            now=True
+        )
