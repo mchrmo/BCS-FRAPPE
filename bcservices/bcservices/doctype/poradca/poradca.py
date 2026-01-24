30496e3620ce32744c7d1e221778e109b8b69a21
AndrejCernak <andrej.cernak2007@gmail.com>
2026-01-24 16:12:16 +0000

    fix fix

--- bcservices/bcservices/doctype/poradca/poradca.py
+++ bcservices/bcservices/doctype/poradca/poradca.py
@@ -1,10 +1,7 @@
 import frappe
-import html
 from frappe.model.document import Document
 
 class Poradca(Document):
-    def after_insert(self):
-        self.send_welcome_email()
 
     @frappe.whitelist()
     def send_welcome_email(self):
@@ -12,19 +9,20 @@ class Poradca(Document):
             frappe.msgprint("Poradca nemá vyplnený email alebo heslo.")
             return
 
-        password_safe = html.escape(self.heslo)
+        subject = "Vaše prihlasovacie údaje"
 
-        subject = "Vaše prihlasovacie údaje – Poradca"
         message = f"""
-        Dobrý deň,<br><br>
-        boli vám vytvorené prihlasovacie údaje do systému.<br><br>
+Dobrý deň,
 
-        📧 <b>Email:</b> {self.email}<br>
-		🔐 <b>Dočasný prístupový kód:</b> {password_safe}<br><br>
+boli vám vytvorené prihlasovacie údaje do systému.
 
-        Prosím, po prihlásení si heslo zmeňte.<br><br>
-        S pozdravom,<br>
-        Váš tím
+Email: {self.email}
+Heslo: {self.heslo}
+
+Prosím, po prihlásení si heslo zmeňte.
+
+S pozdravom,
+Váš tím
         """
 
         frappe.sendmail(
