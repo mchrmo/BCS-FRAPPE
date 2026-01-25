7e041539d8283bf7c8178362458b85515d5741e7
AndrejCernak <andrej.cernak2007@gmail.com>
2026-01-25 14:30:18 +0000

    fix

--- bcservices/bcservices/doctype/poradca/poradca.py
+++ bcservices/bcservices/doctype/poradca/poradca.py
@@ -4,23 +4,28 @@ from frappe.model.document import Document
 
 class Poradca(Document):
 
+    def after_insert(self):
+        # Ak chcete, aby sa mail odoslal hneď po vytvorení poradcu
+        self.send_welcome_email()
+
     @frappe.whitelist()
     def send_welcome_email(self):
+        # Overenie, či sú polia vyplnené
         if not self.email or not self.heslo:
             frappe.msgprint("Poradca nemá vyplnený email alebo heslo.")
             return
 
+        # HTML-safe verzia hesla (prevencia proti špeciálnym znakom v HTML)
+        password_safe = html.escape(str(self.heslo) or "")
+        
         subject = "Vaše prihlasovacie údaje"
-
-       # HTML-safe verzia hesla
-        password_safe = html.escape(self.heslo or "")
-
-        subject = "Vaše prihlasovacie údaje"
+        
+        # Opravená správa: pridané self.email a tag <code> pre lepšiu čitateľnosť
         message = f"""
         Dobrý deň {self.username or 'používateľ'},<br><br>
         boli vám vytvorené prihlasovacie údaje do systému.<br><br>
-        📧 <b>Email:</b> {email}<br>
-        🔐 <b>Heslo:</b> {password_safe}<br><br>
+        📧 <b>Email:</b> {self.email}<br>
+        🔐 <b>Heslo:</b> <code>{password_safe}</code><br><br>
         Prosím, prihláste sa do systému a heslo si v prípade potreby zmeňte.<br><br>
         S pozdravom,<br>
         Váš tím
@@ -32,3 +37,5 @@ class Poradca(Document):
             message=message,
             now=True
         )
+        
+        frappe.msgprint(f"E-mail s údajmi bol odoslaný na adresu {self.email}")
